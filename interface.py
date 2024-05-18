b2701c0a8ebcdc9cd7a1cf827815e453cb599bc8
amineallali <amineallali9.com>
2024-05-18 14:45:28 +0100

    Added Login interface

--- interface.py
+++ interface.py
@@ -1,7 +1,9 @@
 import sys
-from PySide6.QtWidgets import QMessageBox, QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, QPushButton, QWidget, QSpacerItem, QSizePolicy
+from PySide6.QtWidgets import QMessageBox, QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QHBoxLayout, \
+    QPushButton, QWidget, QSpacerItem, QSizePolicy
 from PySide6.QtGui import QPixmap
 import conn
+
 
 class SingupWindow(QMainWindow):
     def __init__(self):
@@ -16,7 +18,7 @@ class SingupWindow(QMainWindow):
 
         # Set the background image
         self.background_label = QLabel(self)
-        pixmap = QPixmap("./photo1.jfif")  # Provide the correct path to your background image
+        pixmap = QPixmap("./bg_image.jpeg")  # Provide the correct path to your background image
         self.background_label.setPixmap(pixmap)
         self.background_label.setGeometry(0, 0, 800, 500)
         self.background_label.setScaledContents(True)
@@ -25,7 +27,6 @@ class SingupWindow(QMainWindow):
         # Create a vertical layout for the form
         form_layout = QVBoxLayout()
         form_layout.setContentsMargins(2, 2, 10, 10)
-        
 
         # Function to add widget with label
         def add_widget_with_label(layout, widget, label_text):
@@ -49,18 +50,16 @@ class SingupWindow(QMainWindow):
         self.name_input.setFixedWidth(350)
         self.confirm_input.setFixedWidth(350)
 
+        # input_style = "background-color: #E8E9EB; color: black; font-size: 20px; border-radius:10px; padding: 10px ;height : 20px;"
+        self.name_input.setStyleSheet("background-color: #E8E9EB; color: black; font-size: 20px; border-radius:10px; padding: 10px ;height : 20px;")
+        self.username_input.setStyleSheet("background-color: #E8E9EB; color: black; font-size: 20px; border-radius:10px; padding: 10px ;height : 20px;")
+        self.password_input.setStyleSheet("background-color: #E8E9EB; color: black; font-size: 20px; border-radius:10px; padding: 10px ;height : 20px;")
+        self.confirm_input.setStyleSheet("background-color: #E8E9EB; color: black; font-size: 20px; border-radius:10px; padding: 10px ;height : 20px;")
 
-        input_style = "background-color: #E8E9EB; color: black; font-size: 20px; border-radius:10px; padding: 10px ;height : 20px;"   
-        self.name_input.setStyleSheet(input_style)
-        self.username_input.setStyleSheet(input_style)
-        self.password_input.setStyleSheet(input_style)
-        self.confirm_input.setStyleSheet(input_style)
-
-       
         # Add the input fields with labels to the form layout
         add_widget_with_label(form_layout, self.name_input, "Name:")
         add_widget_with_label(form_layout, self.username_input, "Username:")
-        add_widget_with_label(form_layout,self. password_input, "Password:")
+        add_widget_with_label(form_layout, self.password_input, "Password:")
         add_widget_with_label(form_layout, self.confirm_input, "Confirm Password:")
 
         # Add a submit button
@@ -68,7 +67,8 @@ class SingupWindow(QMainWindow):
         submit_button.setMaximumWidth(350)  # Set width of the button
         form_layout.addWidget(submit_button)
 
-        submit_button.setStyleSheet("background-color: black; color: white ; border-radius:10px; padding: 10px ; height : 20px;")
+        submit_button.setStyleSheet(
+            "background-color: black; color: white ; border-radius:10px; padding: 10px ; height : 20px;")
 
         # Connect the button to a function (e.g., for processing the sign-in)
         submit_button.clicked.connect(self.handle_sign_up)
@@ -88,11 +88,12 @@ class SingupWindow(QMainWindow):
             cur.execute(sql, params)
 
             conn.connection.commit()
-        
+
             QMessageBox.information(self, "Your account is Successfuly created", f"Welcome, {username}!")
-            
+
         except Exception as e:
             QMessageBox.critical(self, "Error", f"An error occurred: {e}")
+
 
 # Run the application
 app = QApplication(sys.argv)
